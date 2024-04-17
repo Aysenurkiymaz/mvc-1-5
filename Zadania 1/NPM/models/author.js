bcf851d56ac1af54f85821cd4f2180bef0810ea3
Aysenur Kiymaz <kiymaay_aehit@students.vizja.pl>
2024-04-17 17:15:14 +0200

    firsstcommit

--- Zadania 1/NPM/models/author.js
+++
@@ -1,6 +0,0 @@
-const Author = {
-    id: 1,
-    name: 'Author 1'
-  };
-  
-  module.exports = Author;
--- Zadania 1/NPM/models/book.js
+++
@@ -1,8 +0,0 @@
-const Book = {
-    id: 1,
-    title: 'Book 1',
-    publishingYear: 2020,
-    authorId: 1
-  };
-  
-  module.exports = Book;
